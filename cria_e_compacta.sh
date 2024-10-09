16a1de74efa26e956a25df9b5bce1347d4f6305d
Henry-AF <henryfernandes001@gmail.com>
2024-10-09 19:58:35 -0300

    Atualizando recurso

--- cria_e_compacta.sh
+++ cria_e_compacta.sh
@@ -1,11 +1,12 @@
 if [ -z "$1" ]; then
 	echo "Uso: $0 nome_da_pasta"
 	exit 1
+
 fi
 
 mkdir -p "$1"
 
-tar -cvf "1.tar" "$1"
+tar -cvf "Pasta_Compactada.tar" "$1"
 
 echo "A pasta '$1' foi criada e compactada como '$1.tar'."
 
